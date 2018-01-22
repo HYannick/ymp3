5cf5b2deffdb324554197572b5e20f4bf2440479
Yannick Houssin <y.houssin@live.fr>
2018-01-22 15:14:51 +0100

    ignore

--- poi.config.js
+++ poi.config.js
@@ -1,4 +1,6 @@
 const OfflinePlugin = require('offline-plugin')
+// const path = require('path')
+// const PHASER_DIR = path.join(__dirname, '/node_modules/phaser-ce');
 module.exports = options => ({
   entry: './src/index.js',
   devServer: {
@@ -14,4 +16,39 @@ module.exports = options => ({
       pwa: './src/pwa.js' // Use your own runtime, relative to $cwd
     })
   ]
+  // webpack(config) {
+  //   const pix = [
+  //     {
+  //       test: /pixi\.js/,
+  //       use: [{
+  //         loader: 'expose-loader',
+  //         options: 'PIXI',
+  //       }],
+  //     },
+  //     {
+  //       test: /phaser-split\.js$/,
+  //       use: [{
+  //         loader: 'expose-loader',
+  //         options: 'Phaser',
+  //       }],
+  //     },
+  //     {
+  //       test: /p2\.js/,
+  //       use: [{
+  //         loader: 'expose-loader',
+  //         options: 'p2',
+  //       }],
+  //     }
+  //   ]
+  //
+  //   const phaserObj = {
+  //     phaser: path.join(PHASER_DIR, 'build/custom/phaser-split.js'),
+  //     pixi: path.join(PHASER_DIR, 'build/custom/pixi.js'),
+  //     p2: path.join(PHASER_DIR, 'build/custom/p2.js'),
+  //   }
+  //   Object.assign(config.resolve.alias, phaserObj)
+  //   config.module.rules = config.module.rules.concat(pix)
+  //   console.log(config.module.rules)
+  //   return config // <-- Important, must return it
+  // }
 })
